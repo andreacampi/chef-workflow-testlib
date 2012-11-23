d8bb978bd103e5306d80af64c4f282fae5476635
Andrea Campi <andrea.campi@zephirworks.com>
2012-11-23 10:14:32 +0100

    Be more careful accessing an array that might be nil.

--- lib/chef-workflow/helpers/chef.rb
+++ lib/chef-workflow/helpers/chef.rb
@@ -3,6 +3,10 @@ require 'chef/config'
 
 module ChefHelper
   def perform_search(type, query)
-    Chef::Search::Query.new.search(type, query).first.map(&:name)
+    if results = Chef::Search::Query.new.search(type, query).first
+      results.map(&:name)
+    else
+      []
+    end
   end
 end
